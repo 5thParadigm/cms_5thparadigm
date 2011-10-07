d146cec34e078e4ee2ad9ddd737bf88ef8811503
Sean Carroll <sfcarroll@gmail.com>
2011-10-07 20:02:53 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cms5thparadigm::Application.config.secret_token = '0a90eead7cec0968d8962b86fce54c26141e93562ce399ce65e3adc3ceaadfa6d4f79affa426a7d01e42a1e441384e7ba03ac29791de5ecb4f26fcba73ee4571'
